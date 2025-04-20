dc33131cce838c0b8c41e8f3fc52f5b1b14e58a9
Elias Krupa <elias@Eliass-Laptop.local>
2025-04-20 13:01:54 -0500

    outside arduino

--- main/Outside/Outside.ino
+++ main/Outside/Outside.ino
@@ -5,32 +5,34 @@
  * UIN : 650968054                         *
  * NetID : awill276                        *
  *******************************************
- * Developer 2: AJ Williams                *
- * UIN : 650968054                         *
- * NetID : awill276                        *
+ * Developer 2: Elias Krupa                *
+ * UIN : 661040904                         *
+ * NetID : ekrup2                          *
  *******************************************
  * Developer 2: AJ Williams                *
  * UIN : 650968054                         *
  * NetID : awill276                        *
  *******************************************/
 
-SoftwareSerial customSerial(TX, RX);
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3);
 
 /*PURPOSE: */
 void setup() {
   // put your setup code here, to run once:
-  customSerial.begin(9600);
-  Serial.begin(9600);
+  mySerial.begin(38400);
+  Serial.begin(115200);
   //TODO: Begin LCD and other modules
 }
 
 void loop() {
   //Check if data is available
   int numBytes;
-  if (numBytes = customSerial.available()) {
+  if (numBytes = mySerial.available()) {
     byte readBuf[READ_BUFFER_SIZE];
     //Populate the buffer
-    customSerial.readBytes(readBuf, numBytes);
+    mySerial.readBytes(readBuf, numBytes);
 
     ControlHubPackage request;
     if (handleInput(readBuf, numBytes, request)) {
@@ -44,17 +46,29 @@ void loop() {
       }
     }
   }
+
+  // listening for input form control hub arduino
+  if (Serial.available()) {
+    int incomingByte = Serial.read();
+    // handle incoming byte from main Arduino
+    if (debug) {
+      Serial.print("Received from Control Hub: ");
+      Serial.println(incomingByte);
+    }
+  }
+
   //TODO: capture mic, number pad, rfid input. Handle said input
   //TODO: Use speaker if needed.
   //TODO: Begin packaging output for control hub
   bool writing = true;
 
-
   if (writing) {
     //Create struct
 
     //Convert struct to bytes
+
     //Write struct byte by byte
+    mySerial.write('A');
   }
 }
 
